274dbb20dd1b1719ff52995d954c0bd6d611ff77
Gustavo Rodrigues <gustavocr.developer@gmail>
2023-11-10 20:45:43 +0000

    demo functions and objects

--- javascript-typescript/functions-objects/demo-functions.js
+++ javascript-typescript/functions-objects/demo-functions.js
@@ -1,13 +1,51 @@
-function sum(x, y) {
-  return x + y;
-}
+// const z = 10;
+
+// function sum(x, y) {
+//   return x + y + z;
+// }
 
 // Explain function arguments
 // How to invoke a function?
+// const result = sum(1, 2);
+
+// short return
+// console.log(sum(1, 2));
 // Arrow vs normal functions https://www.freecodecamp.org/news/the-difference-between-arrow-functions-and-normal-functions/
 // Arrow functions, explain short return
 // Functional programming
+const sum = (x, y) => x + y;
 // What is return inside a function? Is necessary?
+const print = (msg) => {
+  console.log(msg ?? "Hello world");
+};
+print("Hello world");
+
 // Early return
+
+const getCartItems = (items) => {
+  if (items.length === 0) {
+    return [];
+  }
+
+  print("executa código");
+};
+
+console.log(getCartItems(["item1", "item2"]));
 // What I can return inside a function?
 // Conditionals
+
+const dbUsers = ["user1", "user2", "user3"];
+
+const findUserByName = (name) => {
+  const user = dbUsers.find((user) => user === name);
+
+  if (user) {
+    return user;
+  }
+
+  print();
+
+  return null;
+};
+
+console.log(findUserByName("user4"));
--- javascript-typescript/functions-objects/demo-objects.js
+++ javascript-typescript/functions-objects/demo-objects.js
@@ -8,7 +8,42 @@ const person = {
 };
 
 // Access properties from object person
+console.log(person.firstName);
 // If a property is undefined, what should I do?
+console.log(person.age);
 // How to invoke a function inside object?
+console.log(person.fullName());
 // How to interact over properties?
+Object.keys(person).forEach((key) => {
+  console.log(key);
+  console.log(person[key]);
+});
 // How to interact over values?
+Object.values(person).forEach((value) => {
+  console.log(value);
+});
+
+const getProducts = () => [
+  { name: "Apple", type: "fruit", price: 2, getDiscount: () => 0.1 },
+  { name: "Tomato", type: "vegetable", price: 3 },
+  { name: "Orange", type: "fruit", price: 1 },
+  { name: "Cucumber", type: "vegetable", price: 2 },
+];
+
+const products = getProducts();
+products.forEach((product) => {
+  const { name } = product;
+  // const name = product.name
+  console.log(name);
+  console.log(product.type);
+  console.log(product.price);
+  console.log(product?.getDiscount?.());
+});
+
+// const product1 = products[0];
+// const product2 = products[1];
+const [product1, product2] = products;
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+ */
